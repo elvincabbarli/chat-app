909a9267aef81bdb2da33441e9f36527c26cf7ac
Elvin <elvincabbarl@gmail.com>
2022-08-23 13:45:59 +0400

    login partially done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { auth, db } from "../firebase";
+import { updateDoc, doc } from "firebase/firestore";
+import { signOut } from "firebase/auth";
 
 const NavBar = () => {
+  const handleSignOut = async () => {
+    await updateDoc(db , 'users' , auth.currentUser.uid , {
+      isOnline: false
+    })
+    await signOut(auth)
+  }
   return (
     <nav>
-        <h3>
-            <Link to={'/'}>Messenger</Link>
-        </h3>
-        <div>
-            <Link to={'/registr'}>Registr</Link>
-            <Link to={'/login'}>Login</Link>
-        </div>
+      <h3>
+        <Link to={"/"}>Messenger</Link>
+      </h3>
+      <div>
+        {auth.currentUser ? (
+          <>
+            <Link to={"/profile"}>Profile</Link>
+            <button className="btn" onClick={handleSignOut}>Log Out</button>
+          </>
+        ) : (
+          <>
+            <Link to={"/registr"}>Registr</Link>
+            <Link to={"/login"}>Login</Link>
+          </>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../firebase";
-import { setDoc, doc, Timestamp } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
-const Register = () => {
+const Login = () => {
   const [data, setData] = useState({
-    name: "",
     email: "",
     password: "",
     error: null,
     loading: false,
   });
 
-  console.log(auth.currentUser)
-
-  const { name, email, password, error, loading } = data;
+  const { email, password, error, loading } = data;
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -25,24 +22,15 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setData({ ...data, error: null, loading: true });
-    if (!name || !email || !password) {
+    if (!email || !password) {
       setData({ ...data, error: "All Fields are Required" });
     }
     try {
-      const result = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      await setDoc(doc(db, "users", result.user.uid), {
-        uid: result.user.uid,
-        name,
-        email,
-        createdAt: Timestamp.fromDate(new Date()),
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      await updateDoc(doc(db, "users", result.user.uid), {
         isOnline: true,
       });
       setData({
-        name: "",
         email: "",
         password: "",
         loading: false,
@@ -56,12 +44,8 @@ const Register = () => {
 
   return (
     <section>
-      <h3>Create Your Account</h3>
+      <h3>Log into Your Account</h3>
       <form className="form" action="" onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={handleChange} />
-        </div>
         <div className="input-container">
           <label htmlFor="email">Email</label>
           <input
@@ -82,13 +66,11 @@ const Register = () => {
         </div>
         {error ? <p className="error">{error}</p> : null}
         <div className="btn-container">
-          <button className="btn">
-            {loading ? 'Registering...' : 'Register'}
-          </button>
+          <button className="btn">{loading ? "Loggin in.." : "Log in"}</button>
         </div>
       </form>
     </section>
   );
 };
 
-export default Register;
+export default Login;
